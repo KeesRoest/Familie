6021ebf0fc8e8dd1c91a0362a9b3fb8a94a995ae
Unknown <cproest@hotmail.com>
2016-10-17 15:17:13 +0200

    namenLijst

--- WebContent/JSFuncties.js
+++ WebContent/JSFuncties.js
@@ -11,3 +11,19 @@ function overOns() {
 	  xhttp.open("GET", "overOns.html", true);
 	  xhttp.send();
 	}
+
+function namenLijst() {
+	  var xhttp = new XMLHttpRequest();
+	  xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	      var json = JSON.parse(this.responseText)
+	      var text = ""
+	      for (var i = 0; i < json.length; i++) {
+	        text += "<p>" + json[i].firstName + " " + json[i].lastName + "</p>"
+	      }
+	      document.getElementById("mainpage").innerHTML = text
+	    }
+	  };
+	  xhttp.open("GET", "namenLijst.json", true);
+	  xhttp.send();
+	}
