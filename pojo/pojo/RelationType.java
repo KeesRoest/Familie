0daa85d541c236605e57fbbfa427ef82c48e00d3
Unknown <rcwvandenberg@gmail.com>
2016-11-08 16:06:18 +0100

    Relaties leggen 0.01

---
+++ pojo/pojo/RelationType.java
@@ -0,0 +1,38 @@
+package pojo;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class RelationType  implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+
+	private Long id;
+
+	private Boolean partner;
+	
+	private String relationType;
+
+	public String getRelationType() {
+		return relationType;
+	}
+
+	public void setRelationType(String relationType) {
+		this.relationType = relationType;
+	}
+
+	public Boolean isPartner() {
+		return partner;
+	}
+
+	public void setPartner(Boolean partner) {
+		this.partner = partner;
+	}
+}
---
+++ src/META-INF/load_script.sql
@@ -0,0 +1,5 @@
+INSERT INTO person(id, achternaam, doopnaam, email, geboortedatum, geboorteplaats, geslacht, huisnr, huisnrtoev, land, overlijdensdatum, password, plaatsnaam, postcode, roepnaam, staat, straatnaam, telefoon, tussenvoegsel) 	VALUES (1001, 'Berg', 'Wouter', null, '1915-12-07', 'Oegstgeest', 'M', 0, null, 'Nederland', '1999-11-02', '12345678', null, null, 'Wout', null, null, null, 'van den');
+INSERT INTO person(id, achternaam, doopnaam, email, geboortedatum, geboorteplaats, geslacht, huisnr, huisnrtoev, land, overlijdensdatum, password, plaatsnaam, postcode, roepnaam, staat, straatnaam, telefoon, tussenvoegsel) 	VALUES (1002, 'Berg-van der Krogt', 'Suzanna Maria', null, '1919-06-04', 'Leiden', 'V', 0, null, 'Nederland', '2004-01-11', '12345678', null, null, 'Suus', null, null, null, 'van den');
+INSERT INTO relationtype(id, partner, relationtype) VALUES(1, true, 'Is gehuwd met');
+INSERT INTO relationtype(id, partner, relationtype) VALUES(2, true, 'Is partner van');
+INSERT INTO relationtype(id, partner, relationtype) VALUES(3, false, 'Is kind van');
