3f91743898d22af311868a80bd68d6fe50979f51
Unknown <rcwvandenberg@gmail.com>
2017-01-06 14:47:17 +0100

    Query stamboom

---
+++ src/dao/children.sql
@@ -0,0 +1,37 @@
+select
+       p.id
+,      p.roepnaam
+,      p.tussenvoegsel
+,      p.achternaam
+,      r2.id            as partnerId
+,      r2.roepnaam      as partnerRoepnaam
+,      r2.tussenvoegsel as partnerTussenvoegsel
+,      r2.achternaam    as partnerAchternaam
+from
+       person   p
+inner join
+       relation r
+on
+       r.person1_id  = p.id
+and    r.relatietype = 'Is kind van'
+and    r.person2_id  = 1001
+left join
+       (
+        select
+               r.person1_id
+        ,      r.partner
+        ,      p.id
+        ,      p.roepnaam
+        ,      p.tussenvoegsel
+        ,      p.achternaam
+        from
+               relation r
+        ,      person   p
+        where
+               p.id = r.person2_id
+       ) r2
+on
+       r2.person1_id = p.id
+and    r2.partner    = true
+;
+
